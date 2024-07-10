40c88d64b879e9f81b15d7cd94b92effa5065629
emabytyqi <ema.bytyqi@mileniumi3.net>
2024-07-10 11:50:12 +0200

    vr.1

---
+++ ha.py
@@ -0,0 +1,11 @@
+x= int(input("shkruaj numrin e pikeve:"))
+if((x >= 0) and (x <= 29)):
+    print("ngele")
+elif((x >= 30) and (x <= 49)):
+    print("kaluse")
+elif((x >= 50) and (x <= 69)):
+    print("tresh")
+elif((x >= 70) and (x <= 89)):
+    print("katersh")
+elif((x >= 90) and (x <= 100)):
+    print("shkelqyeshem")
